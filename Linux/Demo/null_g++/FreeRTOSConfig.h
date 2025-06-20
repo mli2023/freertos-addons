45cbea781fe0f9883c7bd073979dfbf27be97053
Matt.Li <mli@ampac.net>
2025-06-20 12:21:56 +0800

    set configUSE_PORT_OPTIMISED_TASK_SELECTION 0, otherwise not working

--- Linux/Demo/null_g++/FreeRTOSConfig.h
+++ Linux/Demo/null_g++/FreeRTOSConfig.h
@@ -86,7 +86,7 @@ extern "C" {
  *----------------------------------------------------------*/
 
 #define configUSE_PREEMPTION					1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION	1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION	0
 #define configUSE_IDLE_HOOK						0
 #define configUSE_TICK_HOOK						1
 #define configTICK_RATE_HZ						( 1000 ) /* In this non-real time simulated environment the tick frequency has to be at least a multiple of the Win32 tick frequency, and therefore very slow. */
